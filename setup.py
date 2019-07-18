205dce758b1be5a72fabdbe79d96c094d5b31593
GuNanHai <42151384+GuNanHai@users.noreply.github.com>
2019-07-18 20:28:13 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'dlpytools',
-    version = '0.0.4',
+    version = '0.0.5',
     keywords='tools',
     description = 'common tools',
     license = 'MIT License',
